702b678de07d3af3052324c90c219b4174563720
North <YangShi2@sf-express.com>
2018-07-01 17:41:24 +0800

    :white_check_mark::art:

---
+++ index.js
@@ -0,0 +1,15 @@
+export {default as Cookie} from './lib/cookie';
+export {default as GetUrlArgs} from './lib/getUrlArgs';
+export {default as Clone} from './lib/Array/clone';
+export {default as Max} from './lib/Array/max';
+export {default as Min} from './lib/Array/min';
+export {default as Sum} from './lib/Array/sum';
+
+/* export default {
+    Cookie: Cookie,
+    GetUrlArgs: GetUrlArgs,
+    Clone,
+    Max,
+    Min,
+    Sum: Sum
+} */
--- test/sum-test.js
+++
@@ -1,4 +0,0 @@
-import Sum from '../Array/sum';
-
-let a = [3, 4, 5, 6, 7];
-Sum(a)();
