f83a16ff73bbea8d88eb8ec80d4e8070e12d947c
North <YangShi2@sf-express.com>
2018-08-22 11:52:17 +0800

    fix test error

--- test/Array/sum.test.js
+++ test/Array/sum.test.js
@@ -6,7 +6,7 @@ it('数组整数求和', () => {
 });
 
 it('数组对象求和', () => {
-    let arr = [{ name: "north", age: 2 }, { age: 4.54 }, { age: "3a" }];
+    let arr = [{ name: "north", age: 2 }, { age: 4.54 }, { age: "3" }];
     let result = Sum(arr)("age");
     expect(result).toBe(9.54);
 });
